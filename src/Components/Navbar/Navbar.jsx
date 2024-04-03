5bc067a24bd6cfe166ec861545584feae98a7e2a
somaiabahaa93 <76841900+somaiabahaa93@users.noreply.github.com>
2024-04-04 00:04:58 +0100

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
   return <>
   <nav className="navbar navbar-expand-lg bg-body-tertiary mb-3 "  style={{backgroundColor:" #e3f2fd"}}>
   <div className="container-fluid">
-    <a class="navbar-brand" href="#">Movies</a>
+    <a class="navbar-brand" href="/">Movies</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
